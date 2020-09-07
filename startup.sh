47c886d8b382413fab93aae1f6e2699bf956ae64
Tola Awofolu <oawofolu@pivotal.io>
2020-09-07 06:30:27 -0400

    Refactored code

--- startup.sh
+++ startup.sh
@@ -2,5 +2,3 @@ export DOCKER_HOST_IP=127.0.0.1
 docker-compose rm -svf
 docker-compose down
 docker-compose up --build
-curl -X DELETE http://localhost:8083/connectors/donation-outbox-connector
-curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ --data-binary "@sql/postgres-connect-config.json"
