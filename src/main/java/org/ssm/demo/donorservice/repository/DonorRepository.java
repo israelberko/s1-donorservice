8b7a6b3b752f000a181a7f5e682aa8473088ba7d
Tola Awofolu <oawofolu@pivotal.io>
2020-09-11 02:26:00 -0400

    Troubleshooting

--- src/main/java/org/ssm/demo/donorservice/repository/DonorRepository.java
+++ src/main/java/org/ssm/demo/donorservice/repository/DonorRepository.java
@@ -5,11 +5,12 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.ssm.demo.donorservice.entity.Donor;
 
 @Repository
 public interface DonorRepository extends PagingAndSortingRepository<Donor, Long>{
 	@Query("DELETE u FROM Donor d WHERE d.pledge_id = :pledge_id")
-	 List<Donor> deleteByPledgeId(UUID pledgeId);
+	 List<Donor> deleteByPledgeId( @Param("pledge_id")  UUID pledgeId);
 }
