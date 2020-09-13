8db481af518e9f64020ccbc2703426ce363a41fb
Tola Awofolu <oawofolu@pivotal.io>
2020-09-13 10:17:50 -0400

    Adding Spring Cloud Stream

--- src/main/java/org/ssm/demo/donorservice/service/DonorOutboxService.java
+++ src/main/java/org/ssm/demo/donorservice/service/DonorOutboxService.java
@@ -1,6 +1,7 @@
 package org.ssm.demo.donorservice.service;
 
 import java.util.UUID;
+import java.util.function.Consumer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,36 +24,68 @@ public class DonorOutboxService {
 	@Autowired DonorService donorService;
 	@Autowired DonorOutboxRepository donorOutboxRepository;
 	
-	@Transactional
-	@KafkaListener(topics = "donor.inbox", groupId = "donor-consumer")
-	public void pledgeRequested(DonorOutbox message) {
-		LOG.info("In donor service: {}", message);
-		
-		Donor donorToSave = donorService.getRandomDonor(message.getEvent_id());
-		
-		applicationEventPublisher.publishEvent(DonorOutbox.from(donorToSave));
-		
-		donorService.save(donorToSave);
-			
-	}
+//	@Transactional
+//	@KafkaListener(topics = "donor.inbox", groupId = "donor-consumer")
+//	public void pledgeRequested(DonorOutbox message) {
+//		LOG.info("In donor service: {}", message);
+//		
+//		Donor donorToSave = donorService.getRandomDonor(message.getEvent_id());
+//		
+//		applicationEventPublisher.publishEvent(DonorOutbox.from(donorToSave));
+//		
+//		donorService.save(donorToSave);
+//			
+//	}
 	
 	@Transactional
-	@KafkaListener(topics = "donor.cancel.inbox", groupId = "donor-consumer")
-	public void pledgeCancelRequested(DonorOutbox message) {
-		LOG.info("In donor service for cancellation: {}", message);
-		
-		UUID pledgeId = message.getEvent_id();
-		
-		boolean deleted = donorService.deleteDonorsByPledgeId(pledgeId);
-		
-		message.setEvent_type(deleted ? 
-				"PLEDGE_CANCEL_REQUESTED_ACK" :
-					"PLEDGE_CANCEL_REQUESTED_NACK");
-		
-		applicationEventPublisher.publishEvent(message);
-		
-		LOG.info("Records deleted: {}", deleted);
+	public Consumer<DonorOutbox> pledgeRequested() {
+		return message -> {
+			LOG.info("In donor service: {}", message);
 			
+			Donor donorToSave = donorService.getRandomDonor(message.getEvent_id());
+			
+			applicationEventPublisher.publishEvent(DonorOutbox.from(donorToSave));
+			
+			donorService.save(donorToSave);
+		};	
+	}
+	
+//	@Transactional
+//	@KafkaListener(topics = "donor.cancel.inbox", groupId = "donor-consumer")
+//	public void pledgeCancelRequested(DonorOutbox message) {
+//		LOG.info("In donor service for cancellation: {}", message);
+//		
+//		UUID pledgeId = message.getEvent_id();
+//		
+//		boolean deleted = donorService.deleteDonorsByPledgeId(pledgeId);
+//		
+//		message.setEvent_type(deleted ? 
+//				"PLEDGE_CANCEL_REQUESTED_ACK" :
+//					"PLEDGE_CANCEL_REQUESTED_NACK");
+//		
+//		applicationEventPublisher.publishEvent(message);
+//		
+//		LOG.info("Records deleted: {}", deleted);
+//			
+//	}
+	
+	@Transactional
+	public Consumer<DonorOutbox> pledgeCancelRequested() {
+		return message -> {
+			LOG.info("In donor service for cancellation: {}", message);
+			
+			UUID pledgeId = message.getEvent_id();
+			
+			boolean deleted = donorService.deleteDonorsByPledgeId(pledgeId);
+			
+			message.setEvent_type(deleted ? 
+					"PLEDGE_CANCEL_REQUESTED_ACK" :
+						"PLEDGE_CANCEL_REQUESTED_NACK");
+			
+			applicationEventPublisher.publishEvent(message);
+			
+			LOG.info("Records deleted: {}", deleted);
+		};
 	}
 
 	@TransactionalEventListener(phase = TransactionPhase.BEFORE_COMMIT)
